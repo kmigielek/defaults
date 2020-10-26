c85dc4bd24a67a6130c3a6ffee001036ff67c0d1
Kazimierz Migiełek <kazimierzmigielek@gmail.com>
2020-10-26 21:46:20 +0100

    sync main improvement

--- sync_drives.sh
+++ sync_drives.sh
@@ -2,6 +2,17 @@
 
 drives=$(ls /Volumes | grep -v Macintosh)
 arr=($(echo $drives | tr " " "\n"))
+main=""
+backup=""
 
-rsync -avr /Volumes/${arr[0]}/Dron /Volumes/${arr[1]}
-rsync -avr /Volumes/${arr[1]}/Dron /Volumes/${arr[0]}
+if [ ${arr[0]} == "Main_1" ];
+then
+    main=${arr[0]}
+    backup=${arr[1]}
+else
+    main=${arr[1]}
+    backup=${arr[0]}
+fi
+
+rsync -avr /Volumes/$main/Dron /Volumes/$backup
+rsync -avr /Volumes/$backup/Dron /Volumes/$main
