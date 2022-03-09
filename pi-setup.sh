ba0fbee93b9d65a048199f8b563778a71ce420e9
kmigielek <kazimierzmigielek@gmail.com>
2022-03-09 16:53:51 +0100

    rpi

--- pi-setup.sh
+++ pi-setup.sh
@@ -4,7 +4,3 @@ sudo apt update -y && sudo apt upgrade -y
 sudo apt install locate zsh -y
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "export PATH=$PATH:/usr/sbin" >> .zshrc
-cp mytheme.zsh-theme ~/.oh-my-zsh/themes/
-sed -i 's/ZSH_THEME.*/ZSH_THEME=mytheme/' ~/.zshrc
-source .zshrc
---
+++ zshrc-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "export PATH=$PATH:/usr/sbin" >> .zshrc
+cp mytheme.zsh-theme ~/.oh-my-zsh/themes/
+sed -i 's/ZSH_THEME.*/ZSH_THEME=mytheme/' ~/.zshrc
+source .zshrc
